7c81292b85a619b8f22dcf1f09dc417a7186fcad
NomanAhmed <officialnoman515@gmail.com>
2024-12-25 14:01:05 +0300

    Added Scroll-Top feature

--- Client/src/AppLayout/AppLayout.jsx
+++ Client/src/AppLayout/AppLayout.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 import { Outlet } from "react-router-dom";
+import ScrollTop from "../utils/ScrollTop.jsx";
 
 const AppLayout = () => {
   return (
     <>
+      <ScrollTop />
       <Header />
       <main className="mt-2 lg:min-h-[70vh] sm:min-h-[58vh] xs:min-h-[61vh]">
         <Outlet />
