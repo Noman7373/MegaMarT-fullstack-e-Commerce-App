617944dd8926f0477cd30ebd6645c7e71f02f15d
NomanAhmed <officialnoman515@gmail.com>
2024-12-28 07:25:28 +0300

    Added UI for Search Product Page

--- Client/src/components/pages/Searchpage.jsx
+++ Client/src/components/pages/Searchpage.jsx
@@ -1,10 +1,22 @@
-import React from "react";
-
+import React, { useState } from "react";
+import CardLoading from "../../utils/CardLoading.jsx";
 const Searchpage = () => {
+  const [searchProduct, setSearchProduct] = useState([]);
+  const [laoding, setLoading] = useState(true);
   return (
-    <div>
-      <h1>Search</h1>
-    </div>
+    <section className="bg-white">
+      <div className="container mx-auto p-4">
+        <p className="font-bold">Search Results: {searchProduct.length} </p>
+      </div>
+      {/* show loading */}
+
+      <div className="grid grid-cols-1 xs:place-items-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 px-10 py-5">
+        {laoding &&
+          new Array(10).fill(null).map((_, index) => {
+            return <CardLoading key={"loadingSearchPage" + index} />;
+          })}
+      </div>
+    </section>
   );
 };
 
--- Client/src/utils/CardLoading.jsx
+++ Client/src/utils/CardLoading.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const CardLoading = () => {
   return (
-    <div className="border p-2 grid gap-1 max-w-32 lg:max-w-52 rounded animate-pulse">
+    <div className="border p-2 grid gap-1 min-w-32 max-w-[13rem] lg:max-w-52 rounded h-[15rem] animate-pulse">
       <div className="min-h-14 bg-blue-50 rounded"></div>
       <div className="p-2 lg:p-3 bg-blue-50 rounded w-20"></div>
       <div className="p-2 lg:p-3 bg-blue-50 rounded"></div>
       <div className="p-2 lg:p-3 bg-blue-50 rounded w-14"></div>
       <div className="flex items-center justify-center gap-3">
-        <div className="p-2 lg:p-3 bg-blue-50 rounded w-20">price</div>
-        <button className="p-2 lg:p-3 bg-blue-50 rounded w-20">price</button>
+        <div className="p-2 lg:p-3 bg-blue-50 rounded w-20"></div>
+        <button className="p-2 lg:p-3 bg-blue-50 rounded w-20"></button>
       </div>
     </div>
   );
