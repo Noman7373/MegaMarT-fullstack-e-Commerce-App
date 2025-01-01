fb798775b4fb698029774676a2ed0b93475327f0
NomanAhmed <officialnoman515@gmail.com>
2025-01-01 10:04:23 +0300

    small chnges cartRoutes.js

--- Server/routes/cartRoutes.js
+++ Server/routes/cartRoutes.js
@@ -10,7 +10,7 @@ import authMiddleware from "../middleware/authMiddleware.js";
 const cartRoutes = Router();
 
 cartRoutes.post("/create/cart", authMiddleware, createCartController);
-cartRoutes.get("/get-cart", authMiddleware, getCartItemsController);
+cartRoutes.get("/get-cart", getCartItemsController);
 cartRoutes.put("/update/cart", updateCartItemsQtyController);
 cartRoutes.delete("/delete/cart", deleteCartItemsController);
 
