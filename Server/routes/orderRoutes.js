a719ebebe7fae92eb16346ab3cdd2e5d4cbc71ae
NomanAhmed <officialnoman515@gmail.com>
2025-01-11 14:50:03 +0300

    Import getOrderHistoryController

--- Server/routes/orderRoutes.js
+++ Server/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
-import { PaymentByCashController } from "../controllers/order.Controller.js";
+import { getOrderHistoryController, PaymentByCashController } from "../controllers/order.Controller.js";
 const orderRoutes = Router();
 
 orderRoutes.post("/payments/cash", authMiddleware, PaymentByCashController);
